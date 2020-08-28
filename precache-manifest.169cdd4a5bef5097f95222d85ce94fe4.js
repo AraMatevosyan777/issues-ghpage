964241877b7d1fa9df681009f4ec39c238d22a30
AraMatevosyan777 <60426627+AraMatevosyan777@users.noreply.github.com>
2020-08-28 16:25:57 +0400

    Updates

---
+++ precache-manifest.169cdd4a5bef5097f95222d85ce94fe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5305ee710c454eb40585e2b16d9bf3a3",
+    "url": "/issues-ghpage/index.html"
+  },
+  {
+    "revision": "d1bed5e6f2498aa5533e",
+    "url": "/issues-ghpage/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "2144c5899fb18eb74402",
+    "url": "/issues-ghpage/static/css/main.4b9f3a28.chunk.css"
+  },
+  {
+    "revision": "d1bed5e6f2498aa5533e",
+    "url": "/issues-ghpage/static/js/2.402fc419.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/issues-ghpage/static/js/2.402fc419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2144c5899fb18eb74402",
+    "url": "/issues-ghpage/static/js/main.990ec52a.chunk.js"
+  },
+  {
+    "revision": "e5b53342d527913c5287",
+    "url": "/issues-ghpage/static/js/runtime-main.3d4205bf.js"
+  }
+]);
